1ff7a7b571fc16a0fcda67c2e1cfccb90a10f8cf
Gabriel Gomes <gasigo10@gmail.com>
2018-12-30 14:37:18 +0000

    feat(project-configuration): create xcode project Create xcode project using single view application template

---
+++ Sky Race/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sky Race
+//
+//  Created by Gabriel Gomes on 30/12/18.
+//  Copyright © 2018 GDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
