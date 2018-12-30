a6d51917f4e44f89818b057570d06873c0d0394d
Gabriel Gomes <gasigo10@gmail.com>
2018-12-30 14:53:24 +0000

    fix(project-configuration): remove unnecessary boiler plate code and headers
    remove AppDelegate's unnecessary functions and remove name and copyright from all file's headers.

--- Sky Race/ViewController.swift
+++ Sky Race/ViewController.swift
@@ -2,9 +2,6 @@
 //  ViewController.swift
 //  Sky Race
 //
-//  Created by Gabriel Gomes on 30/12/18.
-//  Copyright © 2018 GDev. All rights reserved.
-//
 
 import UIKit
 
@@ -12,9 +9,9 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
 	}
-
+	
 
 }
 
