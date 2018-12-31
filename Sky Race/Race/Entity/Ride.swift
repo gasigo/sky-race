3ef12833b177eb21f2f993c38baddfc216e52de7
Gabriel Gomes <gasigo10@gmail.com>
2018-12-31 01:31:08 +0000

    feat(race-detail): update ride entity
    update ride entity to add odds and form

--- Sky Race/Race/Entity/Ride.swift
+++ Sky Race/Race/Entity/Ride.swift
@@ -7,10 +7,14 @@ struct Ride: Codable {
 	let clothNumber: Int
 	let horse: Horse
 	let handicap: String
+	let odds: String
+	let form: String
 	
 	enum CodingKeys: String, CodingKey {
 		case clothNumber = "cloth_number"
 		case horse
 		case handicap
+		case odds = "current_odds"
+		case form = "formsummary"
 	}
 }
