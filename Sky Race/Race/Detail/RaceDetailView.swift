a30fbbe55195e96989f5a0ead82e908d71704301
Gabriel Gomes <gasigo10@gmail.com>
2018-12-31 02:31:50 +0000

    feat(race-detail): create race detail view

--- Sky Race/Race/Detail/RaceDetailView.swift
+++ Sky Race/Race/Detail/RaceDetailView.swift
@@ -5,26 +5,39 @@
 
 import UIKit
 
-class RaceDetailView: UIViewController {
+protocol RaceDetailViewProtocol: class {
+	func load(rides: [Ride])
+}
+
+class RaceDetailView: UIViewController, RaceDetailViewProtocol {
 	@IBOutlet private weak var dateLabel: UILabel!
 	@IBOutlet private weak var scheduleLabel: UILabel!
 	@IBOutlet private weak var tableView: UITableView!
 	private var rides: [Ride] = []
+	private var presenter: RaceDetailPresenterProtocol!
 	
     override func viewDidLoad() {
         super.viewDidLoad()
+		self.presenter.handleLoad(for: self)
     }
-    
-	func set(rides: [Ride]) {
+	
+	func set(presenter: RaceDetailPresenterProtocol) {
+		self.presenter = presenter
+	}
+	
+	func load(rides: [Ride]) {
 		self.rides = rides
+		DispatchQueue.main.async {
+			self.tableView.reloadData()
+		}
 	}
 	
 	@IBAction func didTapOrderButton(_ sender: Any) {
-		
+		self.presenter.handleOrderAction()
 	}
 }
 
-extension RaceDetailView: UITableViewDataSource {
+extension RaceDetailView: UITableViewDataSource, UITableViewDelegate {
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return self.rides.count
 	}
@@ -41,5 +54,9 @@ extension RaceDetailView: UITableViewDataSource {
 						   horseSex: ride.horse.sex,
 						   handicap: ride.handicap)
 		return rideCell
+	}
+	
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		self.presenter.handleHorseSelectAction()
 	}
 }
